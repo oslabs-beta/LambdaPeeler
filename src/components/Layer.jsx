20e4843acc615632f84b2c80e61ad4d517962332
Michael Shand <shandie231@gmail.com>
2023-09-07 14:01:43 -0400

    add layers

--- src/components/Layer.jsx
+++ src/components/Layer.jsx
@@ -1,90 +1,67 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import LinkedFunctions from './LinkedFunctions.jsx';
-import FunctionModal from './FunctionModal.jsx';
 import axios from 'axios';
 
-const Layer = ({ layerName, versionNumber, ARN, functions }) => {
-  // isCollapsed is tracked for each displayed Layer. true (default) means the Layer display is collapsed, false means the Layer box has expanded
+const Layer = ({layerName, versionNumber, ARN, functions}) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
   const [associatedFunctions, setAssociatedFunctions] = useState([]);
 
   const fetchAssociatedFunctions = () => {
-    axios
-      .post(
-        'http://localhost:3000/layers/functions',
-        { ARN: ARN },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-      .then((response) => {
-        setAssociatedFunctions(response.data);
-      })
-      .catch((err) => {
-        console.log('Error:', err);
-      });
-  };
+    axios.post('http://localhost:3000/layers/functions', { ARN: ARN }, {
+      headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key': 'your-rapidapi-key',
+          'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com',
+      }})
+    .then(response => {
+      console.log('layer-functions response: ', response.data)
+      setAssociatedFunctions(response.data);
+    })
+    .catch(err => {
+      console.log('Error:', err)
+    })
+  }
 
   useEffect(() => {
     if (!isCollapsed) {
       fetchAssociatedFunctions();
     }
-  }, [isCollapsed]);
+  }, [isCollapsed])
 
-  const openModal = () => {
-    setIsOpened(true);
-  };
-
-  const closeModal = () => {
-    setIsOpened(false);
-  };
+  
+  const linkFunction = () => {
 
 
   }
 
-
-
-
-
   return (
-    <div id="layer">
-      <button
-        className="collapsible"
-        onClick={() => setIsCollapsed(!isCollapsed)}
-      >
-        <span>
-          {' '}
-          Layer: {layerName}, Ver: {versionNumber}
-          <br></br>
-          ARN: {ARN}
-        </span>
+    <div id='layer'>
+      <button className="collapsible" onClick={() => setIsCollapsed(!isCollapsed)}>
+      <span> Layer: {layerName}, Ver: {versionNumber}, ARN: {ARN}</span>
       </button>
       {!isCollapsed && (
         <div>
+          
           <h3>Functions</h3>
 
           {associatedFunctions.map((element) =>
             
             <div >
-                <LinkedFunctions functionName = {element}  ARN = {ARN} fetch = {fetchAssociatedFunctions}
+                <LinkedFunctions functionName = {element}  ARN = {ARN} fetch = {fetchAssociatedFunctions} functions={functions}
                 />
             </div>               
           )}
 
-          <button onClick={() => openModal()}>Link Function</button>
-          <FunctionModal
-            open={isOpened}
-            closeFunction={closeModal}
-            functions={functions}
-            onSubmit={linkFunction}
-          />
+          <input type='text' placeholder='Function Name'></input>  
+          <input type='text' placeholder='ARN'></input>  
+          <button onClick={() => linkFunction()}>Link Function</button>
+
         </div>
+
       )}
     </div>
-  );
-};
+  )
+}
 
 export default Layer;
