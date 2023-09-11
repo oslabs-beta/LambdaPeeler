9a4feba328efaf15f68ae005f4a6524e53d740a2
Zach Hamilton <zachmhamilton@outlook.com>
2023-09-11 10:16:07 -0400

    auth structure

---
+++ server/controllers/userController.js
@@ -0,0 +1,76 @@
+// import bcrypt
+// import jwt
+// import db
+// import env config
+const userController = {};
+
+userController.createUser = (req, res, next) => {
+  // pull user/pass off req.body
+  try {
+    // use bcrypt.hash to hash password
+    // insert into db using user and hash
+    // return next
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+};
+
+userController.verifyUser = (req, res, next) => {
+  // pull user/pass off req.body
+  try {
+    // find user in db
+    // pull password out of user obj
+    try {
+      // use bcrypt.compare to check password
+      // if it doesnt match
+      // return next with err message
+      // return next
+    } catch (err) {
+      console.log(err);
+      return next(err);
+    }
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+};
+
+userController.createToken = (req, res, next) => {
+  // pull user off res.locals
+  try {
+    // find user in db
+    // use jwt.sign on user obj with secret env key
+    // create cookie with token
+    // give this an expiration to persist session?
+    // ex. delete when they logout
+    // and delete after an hour
+    // return next
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+};
+
+userController.verifyToken = (req, res, next) => {
+  // pull token from cookies
+  try {
+    // use jwt.verify to check if token is valid with secret env key
+    // return next
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+};
+
+userController.deleteToken = (res, res, next) => {
+  try {
+    // use res.clearCookie to delete both cookies
+    // return next
+  } catch (err) {
+    console.log(err);
+    return next(err);
+  }
+};
+
+module.exports = userController;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const layerRouter = require('./routes/layerRouter')
-const functionRouter = require('./routes/functionRouter')
+const layerRouter = require('./routes/layerRouter');
+const functionRouter = require('./routes/functionRouter');
+const userRouter = require('./routes/userRouter');
 
 // Initialize Express
 const app = express();
@@ -9,18 +10,13 @@ const PORT = 3000;
 // CORS
 const cors = require('cors');
 app.use(cors());
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // Endpoint to list AWS Lambda layers
-app.use('/layers', layerRouter)
-app.use('/functions', functionRouter)
-
-
-
-
-
+app.use('/layers', layerRouter);
+app.use('/functions', functionRouter);
+app.use('/user', userRouter);
 
 // Start Express Server
 app.listen(PORT, () => {
