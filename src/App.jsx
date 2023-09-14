d8a65f10e3d12c78c0955ef486636c7b9b6943db
Zach Hamilton <zachmhamilton@outlook.com>
2023-09-14 12:40:50 -0400

    created login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./components/Main.jsx";
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Main from './components/Main.jsx';
+import Login from './components/Login.jsx';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-  <BrowserRouter>
-    <Routes>
-      <Route  path="/" element = {<Main />} />
-    </Routes>
-  </BrowserRouter>
-  )};
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={isLoggedIn ? <Main /> : <Navigate replace to={'/login'} />}
+        />
+        <Route
+          path="/login"
+          element={
+            !isLoggedIn ? (
+              <Login setIsLoggedIn={setIsLoggedIn} />
+            ) : (
+              <Navigate replace to={'/'} />
+            )
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
